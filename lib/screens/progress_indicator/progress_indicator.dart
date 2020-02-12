1b060d8b6d3f4c865d80dea62f201b7f93507407
Shravya.ckm <shravya@pietechsolution.com>
2020-02-12 17:50:11 +0530

    progress bar

--- lib/screens/progress_indicator/progress_indicator.dart
+++ lib/screens/progress_indicator/progress_indicator.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:getflutter/getflutter.dart';
+import 'package:getflutter/components/progress_indicator/progress_bar.dart';
+import 'package:getflutter/components/progress_indicator/circular.dart';
+
 
 class ProgressBar extends StatefulWidget {
   @override
@@ -22,30 +25,66 @@ class _ProgressBarState extends State<ProgressBar> {
             ),
           ),
           title: const Text(
-            'Progress Indicator',
+            'Progress Bar',
             style: TextStyle(fontSize: 17),
           ),
           centerTitle: true,
         ),
         body: ListView(
           children: <Widget>[
-            GFProgressIndicator(
+            GFProgressBar(
 //          width: 140.0,
-          linearGradient: LinearGradient(
-            colors: [Colors.red, Colors.red],
-          ),
+//          linearGradient: LinearGradient(
+//            colors: [Colors.red, Colors.red],
+//          ),
           lineHeight: 14.0,
-          percent: 1,
+          progressPercent: 0.6,
           center: Text(
-            "100%",
-            style: TextStyle(fontSize: 12.0),
+            "20%", textAlign: TextAlign.end,
+            style:  TextStyle(fontSize: 12.0,),
           ),
           trailing: Icon(Icons.mood),
-          linearStrokeCap: LinearStrokeCap.butt,
+          linearStrokeCap: LinearStrokeCap.roundAll,
           backgroundColor: Colors.grey,
+              type: GFProgressType.linear,
+              progressColor: Colors.green,
+
         ),
 
+            GFProgressBar(
+//          width: 140.0,
+//          linearGradient: LinearGradient(
+//            colors: [Colors.red, Colors.red],
+//          ),
+              lineHeight: 14.0,
+              progressPercent: 0.6,
+              center: Text(
+                "20%", textAlign: TextAlign.end,
+                style:  TextStyle(fontSize: 12.0,),
+              ),
+              linearStrokeCap: LinearStrokeCap.roundAll,
+              backgroundColor: Colors.grey,
+              type: GFProgressType.round,
+              progressColor: Colors.green,
 
+            ),
+
+
+
+//            CircularPercentIndicator(
+//              radius: 100.0,
+//              lineWidth: 10.0,
+//              percent: 0.6,
+//              center: Text("60%"),
+////              circularStrokeCap: CircularStrokeCap.round,
+//              backgroundColor: Colors.grey,
+//              maskFilter: MaskFilter.blur(BlurStyle.solid, 3),
+//              linearGradient: LinearGradient(
+//                begin: Alignment.topCenter,
+//                end: Alignment.bottomCenter,
+//                colors: [Colors.orange, Colors.yellow],
+//              ),
+//            ),
           ],
         ),
       );
