a86a2386e56c08e14dee0f76f01958147bc6d6ca
Shravya.ckm <shravya@pietechsolution.com>
2020-02-14 17:39:21 +0530

    formating code

--- lib/screens/progress_indicator/progress_indicator.dart
+++ lib/screens/progress_indicator/progress_indicator.dart
@@ -4,7 +4,6 @@ import 'package:getflutter/getflutter.dart';
 import 'package:getflutter/components/progress_indicator/progress_bar.dart';
 import 'package:getflutter/components/progress_indicator/circular.dart';
 
-
 class ProgressBar extends StatefulWidget {
   @override
   _ProgressBarState createState() => _ProgressBarState();
@@ -27,11 +26,11 @@ class _ProgressBarState extends State<ProgressBar> {
           title: const Text(
             'Progress Bar',
             style: TextStyle(fontSize: 17),
-          ), centerTitle: true,
+          ),
+          centerTitle: true,
         ),
         body: ListView(
           children: <Widget>[
-
             const Padding(
               padding: EdgeInsets.only(left: 15, top: 30),
               child: GFTypography(
@@ -42,58 +41,76 @@ class _ProgressBarState extends State<ProgressBar> {
               ),
             ),
             Container(
-            margin: EdgeInsets.only(left: 10, right: 10, top: 20),
-              child: GFProgressBar(
-                percentage: 0.8,
-                radius: 70,
-                lineHeight: 20.0,
-                alignment: MainAxisAlignment.spaceBetween,
-
-                child:  Text(
-                  "80%", textAlign: TextAlign.end,
-                  style:  TextStyle(fontSize: 16.0, color: Colors.white),
-                ),
-//                trailing: Icon(Icons.mood),
-//                leading: Icon(Icons.message),
-
-                backgroundColor: Colors.black26,
-                type: GFProgressType.linear,
-                progressBarColor: Colors.green,
-                animation: true,
-                animationDuration: 3000,
-//                headType: GFProgressHeadType.square,
-//                progressHeadType: GFProgressHeadType.square,
-//                linearStrokeCap: LinearStrokeCap.round,
-                progressHeadType: GFProgressHeadType.square,
-              ),
-            ),
-
-            Container(
               margin: EdgeInsets.only(left: 10, right: 10, top: 20),
               child: GFProgressBar(
-                percentage: 1,
-                radius: 70,
+                percentage: 0.8,
                 lineHeight: 20.0,
                 alignment: MainAxisAlignment.spaceBetween,
 
-                child:  Padding(padding: EdgeInsets.only(right: 5), child: Text(
-                  "100%", textAlign: TextAlign.end,
-                  style:  TextStyle(fontSize: 16.0, color: Colors.white),
-                ),),
-//                trailing: Icon(Icons.mood),
-//                leading: Icon(Icons.message),
-//                progressHeadType: GFProgressHeadType.circular,
-                linearStrokeCap: LinearStrokeCap.butt,
-                backgroundColor: Colors.black,
+                child: Text(
+                  "80%",
+                  textAlign: TextAlign.end,
+                  style: TextStyle(fontSize: 16.0, color: Colors.white),
+                ),
+
+                leading: Icon(
+                  Icons.sentiment_dissatisfied,
+                  color: GFColors.DANGER,
+                ),
+                trailing: Icon(
+                  Icons.sentiment_satisfied,
+                  color: GFColors.SUCCESS,
+                ),
+                backgroundColor: Colors.black26,
                 type: GFProgressType.linear,
-                progressBarColor: Colors.red,
+                progressBarColor: GFColors.INFO,
+                animation: true,
+                animationDuration: 4000,
+//
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 10, right: 10, top: 30),
+              child: GFProgressBar(
+                percentage: 0.3,
+                lineHeight: 20.0,
+                alignment: MainAxisAlignment.spaceBetween,
+                child: Text(
+                  "30%",
+                  textAlign: TextAlign.end,
+                  style: TextStyle(fontSize: 16.0, color: Colors.white),
+                ),
+                backgroundColor: Colors.black26,
+                type: GFProgressType.linear,
+                progressBarColor: GFColors.DANGER,
+                animation: true,
+                animationDuration: 2000,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 10, right: 10, top: 30),
+              child: GFProgressBar(
+                percentage: 0.5,
+                lineHeight: 20.0,
+                alignment: MainAxisAlignment.spaceBetween,
+
+                child: Padding(
+                  padding: EdgeInsets.only(right: 5),
+                  child: Text(
+                    "50%",
+                    textAlign: TextAlign.end,
+                    style: TextStyle(fontSize: 16.0, color: Colors.white),
+                  ),
+                ),
+//
+                progressHeadType: GFProgressHeadType.square,
+                backgroundColor: Colors.black26,
+                type: GFProgressType.linear,
+                progressBarColor: GFColors.WARNING,
                 animation: true,
                 animationDuration: 3000,
               ),
             ),
-
-
-
             const Padding(
               padding: EdgeInsets.only(left: 15, top: 30, bottom: 50),
               child: GFTypography(
@@ -104,62 +121,49 @@ class _ProgressBarState extends State<ProgressBar> {
               ),
             ),
             Container(
-//              height: 120,
-//              width: 300,
-//            margin: EdgeInsets.only(top:20),
               child: GFProgressBar(
-                width: 300,
-              alignment: MainAxisAlignment.spaceBetween,
-
-radius: 90,
+//                width: 300,
+                width: 100,
+                alignment: MainAxisAlignment.spaceBetween,
+                radius: 90,
                 percentage: 0.5,
                 lineHeight: 20.0,
-circleWidth: 10,
-
+                circleWidth: 10,
                 child: Text(
-                  "50%", textAlign: TextAlign.end,
-                  style:  TextStyle(fontSize: 18.0,),
+                  "50%",
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
                 ),
-//                linearBarType: LinearBarType.roundAll,
                 backgroundColor: Colors.black26,
                 type: GFProgressType.round,
-                progressBarColor: Colors.purple,
-//headType:GFProgressHeadType.circular,
+                progressBarColor: GFColors.SECONDARY,
                 animation: true,
-                animationDuration: 3000,
-                progressHeadType: GFProgressHeadType.square,
+                animationDuration: 2500,
               ),
             ),
-
-
             Container(
-//              height: 120,
-//              width: 300,
-            margin: EdgeInsets.only(top:80),
+              margin: EdgeInsets.only(top: 90),
               child: GFProgressBar(
                 width: 100,
                 circleStartAngle: 20,
-
                 alignment: MainAxisAlignment.spaceBetween,
-
                 radius: 90,
                 percentage: 0.6,
                 lineHeight: 20.0,
                 circleWidth: 10,
-
                 child: Text(
-                  "60%", textAlign: TextAlign.end,
-                  style:  TextStyle(fontSize: 18.0),
+                  "60%",
+                  textAlign: TextAlign.end,
+                  style: TextStyle(fontSize: 18.0),
                 ),
-//                linearBarType: LinearBarType.roundAll,
                 backgroundColor: Colors.black26,
                 type: GFProgressType.round,
-                progressBarColor: Colors.teal,
+                progressBarColor: GFColors.PRIMARY,
                 progressHeadType: GFProgressHeadType.circular,
-
                 animation: true,
-                animationDuration: 3000,
-
+                animationDuration: 2800,
               ),
             ),
           ],
